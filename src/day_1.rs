639ff0d3dfb873364e5b70538061259c71d8354e
yijiahe <yi.jiahe00@gmail.com>
2023-12-01 06:30:24 +0000

    Implement get_calibration_value_part_2

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 /// Returns a 2 digit integer given a string containing at least one digit
 ///
 /// # Arguments
@@ -36,6 +38,45 @@ pub fn get_sum_of_calibration_values_in_document(document: &str) -> i32 {
 /// # Arguments
 ///
 /// * `line` a line from the newly-improved calibration document
-pub fn get_calibration_value_part_2(line: &str) -> i32 {
-    0
+pub fn get_calibration_value_part_2(line: &str) -> usize {
+    let (mut first_digit_position, mut first_digit): (Option<usize>, Option<char>) = (None, None);
+    let (mut last_digit_position, mut last_digit): (Option<usize>, Option<char>) = (None, None);
+    for (i, c) in line.chars().enumerate() { 
+        if c.is_digit(10) {
+            if first_digit.is_none() {
+                first_digit_position = Some(i);
+                first_digit = Some(c);
+            }
+            last_digit_position = Some(i);
+            last_digit = Some(c);
+        }
+    }
+
+    let letters_digits = HashMap::from([
+        ("one", '1'),
+        ("two", '2'),
+        ("three", '3'),
+        ("four", '4'),
+        ("five", '5'),
+        ("six", '6'),
+        ("seven", '7'),
+        ("eight", '8'),
+        ("nine", '9')
+    ]);
+
+    for (letters, digit) in &letters_digits {
+        let position = line.find(letters);
+        if !position.is_none() {
+            if first_digit_position.is_none() || position < first_digit_position {
+                first_digit_position = position;
+                first_digit = Some(*digit);
+            }
+            if first_digit_position.is_none() || position > last_digit_position {
+                last_digit_position = position;
+                last_digit = Some(*digit);
+            }
+        }
+    }
+
+    format!("{}{}", first_digit.unwrap(), last_digit.unwrap()).parse::<usize>().unwrap()
 }
