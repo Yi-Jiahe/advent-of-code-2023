bf3f3a7ea9bfc35501e5aaf6bceb4a70f5bfe268
yijiahe <yi.jiahe00@gmail.com>
2023-12-02 06:14:04 +0000

    Complete day 2 part 2

--- src/day_2.rs
+++ src/day_2.rs
@@ -55,6 +55,54 @@ pub fn day_2_get_sum_of_possible_game_ids(games: &str) -> usize {
     ans
 }
 
+/// Returns the product of the minimum number of each colour of cube required for each game.
+/// # Arguments
+///
+/// * `game` String containing the game ID and sets of cubes oulled out in the game
+///
+/// # Returns
+/// Product of the minimum number of each colour of cube required
+fn minimum_power(game: &str) -> usize {
+    let game_re = Regex::new(r"^.*:(.*)$").unwrap();
+    let caps = game_re.captures(game).unwrap();
+
+    let sets = caps.get(1).unwrap().as_str().split(";");
+    let cube_res = [
+        Regex::new(r"(\d+) red").unwrap(),
+        Regex::new(r"(\d+) green").unwrap(),
+        Regex::new(r"(\d+) blue").unwrap(),
+    ];
+    let mut min_cubes = [0, 0, 0];
+    for set in sets {
+        for (i, re) in cube_res.iter().enumerate() {
+            let caps = re.captures(set);
+            if caps.is_none() { continue; }
+            let n = caps.unwrap().get(1).unwrap().as_str().parse::<usize>().unwrap();
+            if n > min_cubes[i] {
+                min_cubes[i] = n
+            }
+        }
+    }
+
+    min_cubes[0] * min_cubes[1] * min_cubes[2]
+}
+
+/// Determines the sum of the power of minimum set of cubes that must have been present for each game given information about each game
+/// # Arguments
+///
+/// * `games` String containing information about each game
+///
+/// # Returns
+/// Sum of the power of minimum set of cubes that must have been present for each game
+pub fn day_2_get_sum_of_minimum_power(games: &str) -> usize {
+    let mut ans = 0;
+
+    for game in games.split("\n") {
+        ans = ans + minimum_power(game.trim());
+    }
+    ans
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -79,5 +127,25 @@ mod tests {
         test_game_is_valid_3: (("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", [12, 13, 14]), (3, false)),
         test_game_is_valid_4: (("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", [12, 13, 14]), (4, false)),
         test_game_is_valid_5: (("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", [12, 13, 14]), (5, true)),
+    }
+
+    macro_rules! generate_minimum_power_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (game, expected) = $value;
+                assert_eq!(expected, minimum_power(game));
+            }
+        )*
+        }
+    }
+
+    generate_minimum_power_tests! {
+        test_minimum_power_1: ("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 48),
+        test_minimum_power_2: ("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 12),
+        test_minimum_power_3: ("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", 1560),
+        test_minimum_power_4: ("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", 630),
+        test_minimum_power_5: ("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 36),
     }
 }
--- tests/day_2.rs
+++ tests/day_2.rs
@@ -2,6 +2,7 @@
 mod day_2_tests {
     use aoc_restore_snow_operations::day_2::{
         day_2_get_sum_of_possible_game_ids,
+        day_2_get_sum_of_minimum_power
     };
 
     #[test]
@@ -9,6 +10,20 @@ mod day_2_tests {
         assert_eq!(
             8,
             day_2_get_sum_of_possible_game_ids(
+                r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"#
+            )
+        );
+    }
+
+    #[test]
+    fn test_day_2_get_sum_of_minimum_power() {
+        assert_eq!(
+            2286,
+            day_2_get_sum_of_minimum_power(
                 r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
                 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
                 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
