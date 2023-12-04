86e1614de7001164c67cf327c0c89134d0180e8a
yijiahe <yi.jiahe00@gmail.com>
2023-12-04 03:09:22 +0000

    Finish day 3 part 1

--- src/day_3.rs
+++ src/day_3.rs
@@ -1,8 +1,7 @@
-use regex::Regex;
-
 fn load_schematic(schematic: &str) -> Vec<Vec<char>> {
     schematic
         .split("\n")
+        .map(|line| line.trim().chars().collect())
         .collect()
 }
 
@@ -11,47 +10,83 @@ pub fn day_3_get_sum_of_part_numbers(schematic: &str) -> usize {
     let indexable_schematic = load_schematic(schematic);
     let n = indexable_schematic.len();
     let m = indexable_schematic[0].len();
-    for (i, line) in schematic.split("\n").enumerate() {
-        let re = Regex::new(r"(\d+)").unwrap();
-        for (s, e, number) in re
-            .find_iter(line)
-            .map(|m| (m.start(), m.end(), m.as_str().parse::<usize>().unwrap()))
-        {
-            let u = if i >= 1 { i - 1 } else { i };
-            let d = if i < n - 1 { i + i } else { i };
-            let l = if s >= 1 { s - 1 } else { s };
-            let r = if e < m - 1 { e } else { e - 1 };
+    for (i, line) in indexable_schematic.iter().enumerate() {
+        let mut s = 0;
+        let mut number_str: String = "".to_string();
+        for (j, c) in line.iter().enumerate() {
+            if c.is_digit(10) {
+                if number_str == "" {
+                    s = j;
+                }
+                number_str = format!("{}{}", number_str, c);
+                
+                if (j == m - 1) || !indexable_schematic[i][j+1].is_digit(10) {
+                    let number = number_str.parse::<usize>().unwrap();
+                    let e = j;
 
-            dbg!(&s, &e, &number);
-            dbg!(&l, &r);
+                    let u = if i >= 1 { i - 1 } else { i };
+                    let d = if i < n - 1 { i+1 } else { i };
+                    let l = if s >= 1 { s - 1 } else { s };
+                    let r = if e < m - 1 { e+1 } else { e };
+       
+                    dbg!(&s, &e, &number);
+                    dbg!(&u, &d, &l, &r);
+                    // let include = 'block: {
+                    //     for y in u..d {
+                    //         dbg!(&y);
+                    //         if y == i {
+                    //             for x in [l, r] {
+                    //                 let c = indexable_schematic[y][x];
+                    //                 if !c.is_digit(10) && c != '.' {
+                    //                     break 'block true;
+                    //                 }
+                    //             }
+                    //         } else {
+                    //             for x in l..=r {
+                    //                 let c = indexable_schematic[y][x];
+                    //                 if !c.is_digit(10) && c != '.' {
+                    //                     break 'block true;
+                    //                 }
+                    //             }
+                    //         }
+                    //     }
+                    //     false
+                    // };
 
-            let include = 'block: {
-                for y in u..d {
-                    dbg!(&y);
-                    if y == i {
-                        for x in [l, r] {
-                            let c = indexable_schematic[y][x];
-                            if !c.is_digit(10) && c != '.' {
-                                break 'block true;
+                    let mut include = false;
+                    for y in u..=d {
+                        dbg!(&y);
+                        if y == i {
+                            for x in [l, r] {
+                                let c = indexable_schematic[y][x];
+                                if !c.is_digit(10) && c != '.' {
+                                    include = true;
+                                    break;
+                                }
+                                if include { break; }
                             }
-                        }
-                    } else {
-                        for x in l..=r {
-                            let c = indexable_schematic[y][x];
-                            if !c.is_digit(10) && c != '.' {
-                                break 'block true;
+                        } else {
+                            for x in l..=r {
+                                let c = indexable_schematic[y][x];
+                                if !c.is_digit(10) && c != '.' {
+                                    include = true;
+                                    break;
+                                }
+                                if include { break; }
                             }
                         }
                     }
-                }
-                false
-            };
+        
+                    dbg!(&include);
+                    if include { ans = ans + number; }
 
-            if include { ans = ans + number; }
+                    number_str = "".to_string();
+                }
+            }
         }
     }
 
-    0
+    ans
 }
 
 #[cfg(test)]
