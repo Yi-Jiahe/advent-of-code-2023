7379b839321d75f7c2c422ac2f9696af22a43e1a
yijiahe <yi.jiahe00@gmail.com>
2023-12-04 06:54:20 +0000

    Finish day 3 part 2

--- src/day_3.rs
+++ src/day_3.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 fn load_schematic(schematic: &str) -> Vec<Vec<char>> {
     schematic
         .split("\n")
@@ -31,7 +33,6 @@ pub fn day_3_get_sum_of_part_numbers(schematic: &str) -> usize {
        
                     let include = 'block: {
                         for y in u..=d {
-                            dbg!(&y);
                             if y == i {
                                 for x in [l, r] {
                                     let c = indexable_schematic[y][x];
@@ -62,11 +63,95 @@ pub fn day_3_get_sum_of_part_numbers(schematic: &str) -> usize {
     ans
 }
 
+#[derive(Debug)]
+struct Part {
+    row: usize,
+    start: usize,
+    end: usize,
+    number: usize,
+}
+
+fn extract_parts(schematic: &str) -> HashMap<usize, Vec<Part>> {
+    let mut parts: HashMap<usize, Vec<Part>> = HashMap::new();
+    for (i, line) in schematic.split("\n").map(|line| line.trim()).enumerate() {
+        let mut parts_on_line = Vec::new();
+        let mut s = 0;
+        let mut number_str: String = "".to_string();
+        let chars: Vec<char> = line.chars().collect();
+        let m = chars.len();
+        for j in 0..m {
+            let c = chars[j];
+            if c.is_digit(10) {
+                if number_str == "" {
+                    s = j;
+                }
+                number_str = format!("{}{}", number_str, c);
+                         
+                if (j == m - 1) || !chars[j+1].is_digit(10) {
+                    parts_on_line.push(Part {
+                        row: i,
+                        start: s,
+                        end: j,
+                        number: number_str.parse::<usize>().unwrap(),
+                    });
+                    number_str = "".to_string();
+                }
+            }
+        }
+        if parts_on_line.len() != 0 {
+            parts.insert(i, parts_on_line);
+        }
+    }
+
+    parts
+}
+
+pub fn day_3_get_sum_of_gear_ratios(schematic: &str) -> usize {
+    let mut ans = 0;
+
+    let parts = extract_parts(schematic);
+
+    for (i, line) in schematic.split("\n").map(|line| line.trim()).enumerate() {
+        let m = line.len();
+        for (j, c) in line.chars().enumerate() {
+            if c == '*' {
+                let mut adjacent_parts = HashSet::new();
+
+                for y in i-1..=i+1 {
+                    for x in j-1..=j+1 {
+                        if y == i && x == j {
+                            continue;
+                        }
+                        let parts_on_line = parts.get(&y);
+                        if parts_on_line.is_none() {
+                            continue;
+                        }
+                        for part in parts_on_line.unwrap() {
+                            if x >= part.start && x <= part.end {
+                                adjacent_parts.insert(part.number);
+                            }
+                        }
+                    }
+                }
+                if adjacent_parts.len() == 2 {
+                    let mut a = 1;
+                    for part in adjacent_parts {
+                        a = a * part;
+                    }
+                    ans = ans + a; 
+                }
+            }
+        }
+    }
+
+    ans
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const example_schematic: &str = r#"467..114..
+    const EXAMPLE_SCHEMATIC: &str = r#"467..114..
     ...*......
     ..35..633.
     ......#...
@@ -79,6 +164,11 @@ mod tests {
 
     #[test]
     fn test_day_3_get_sum_of_part_numbers() {
-        assert_eq!(4361, day_3_get_sum_of_part_numbers(example_schematic));
+        assert_eq!(4361, day_3_get_sum_of_part_numbers(EXAMPLE_SCHEMATIC));
+    }
+
+    #[test]
+    fn test_day_3_get_sum_of_gear_ratios() {
+        assert_eq!(467835, day_3_get_sum_of_gear_ratios(EXAMPLE_SCHEMATIC));
     }
 }
