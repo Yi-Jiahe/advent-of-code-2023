a6abdcb32466f43773e457627a8dc274a100706c
Yi Jiahe <yi_jiahe@tech.gov.sg>
2023-12-05 17:50:23 +0800

    Fix errors for day_5

--- src/day_5.rs
+++ src/day_5.rs
@@ -4,22 +4,35 @@ use std::collections::HashMap;
 
 use regex::Regex;
 
-fn parse_almanac(almanac: &str) -> (Vec<usize>, HashMap<&str, Vec<(usize, usize, usize)>>, HashMap<&str: &str>) {
+fn parse_almanac(
+    almanac: &str,
+) -> (
+    Vec<usize>,
+    HashMap<&str, Vec<(usize, usize, usize)>>,
+    HashMap<&str, &str>,
+) {
     let almanac_iterator = almanac.split("\n\n").map(|section| section.trim());
 
     // Consume first line which contains a listing of which seeds need to be planted
     let seeds_line = almanac_iterator.next().unwrap();
     let re = Regex::new(r"^seeds:(.*)$").unwrap();
     let caps = re.captures(seeds_line).unwrap();
-    let seeds = caps.get(1).unwrap().as_str().trim().split_whitespace().map(|x| x.trim().parse::<usize>().unwrap()).collect();
+    let seeds = caps
+        .get(1)
+        .unwrap()
+        .as_str()
+        .trim()
+        .split_whitespace()
+        .map(|x| x.trim().parse::<usize>().unwrap())
+        .collect();
 
     let mut number_map: HashMap<&str, Vec<(usize, usize, usize)>> = HashMap::new();
-    let mut category_map: HashMap<&str: &str> = HashMap::new();
+    let mut category_map: HashMap<&str, &str> = HashMap::new();
 
     // The rest of the almanac contains a list of maps which describe how to convert numbers from a source category into numbers in a destination category.
     let re = Regex::new(r"^(\w+)-to-(\w+) map:$").unwrap();
     for map in almanac_iterator {
-        let map_iterator = almanac_iterator.split("\n").map(|line| line.trim());
+        let map_iterator = map.split("\n").map(|line| line.trim());
 
         let category_line = map_iterator.next().unwrap();
 
@@ -27,14 +40,21 @@ fn parse_almanac(almanac: &str) -> (Vec<usize>, HashMap<&str, Vec<(usize, usize,
 
         let l = caps.get(1).unwrap().as_str();
         let r = caps.get(2).unwrap().as_str();
-        category_map.push(l, r)
+        category_map.insert(l, r);
 
-        let mappings: Vec<(usize, usize, usize) = Vec::new();
+        let mappings: Vec<(usize, usize, usize)> = Vec::new();
         for mapping in map_iterator {
-            mappings.push(mapping.split_whitespace().map(|x| x.trim().parse::<usize>().unwrap()).next_tuple<(usize, usize, usize)>())
+            let numbers = mapping
+                .split_whitespace()
+                .map(|x| x.trim().parse::<usize>().unwrap());
+            let destination_range_start = numbers.next().unwrap();
+            let source_range_start = numbers.next().unwrap();
+            let range_length = numbers.next().unwrap();
+
+            mappings.push((destination_range_start, source_range_start, range_length));
         }
-        mappings.sort_by(|a, b| a.0.cmp(b.0));
-        number_map.push(l, mappings);
+        mappings.sort_by(|a, b| a.1.cmp(&b.1));
+        number_map.insert(l, mappings);
     }
 
     (seeds, number_map, category_map)
@@ -46,7 +66,8 @@ mod tests {
 
     #[test]
     fn test_parse_almanac() {
-        let (seeds, number_map, category_map) = parse_almanac(r#"seeds: 79 14 55 13
+        let (seeds, number_map, category_map) = parse_almanac(
+            r#"seeds: 79 14 55 13
 
         seed-to-soil map:
         50 98 2
@@ -78,8 +99,9 @@ mod tests {
         
         humidity-to-location map:
         60 56 37
-        56 93 4"#);
+        56 93 4"#,
+        );
 
-        assert_eq!([79, 14, 55, 13], seeds);
+        assert_eq!(vec!([79, 14, 55, 13]), seeds);
     }
 }
