2f2c8ab7de5137cb819dd9228fe371924ef81aa9
Jiahe <yi.jiahe00@gmail.com>
2023-12-17 10:06:31 +0800

    Pass day 16 part 1 example

---
+++ src/day_16.rs
@@ -0,0 +1,114 @@
+use crate::utils::{move_2d, parse_2d_matrix, print_2d_matrix};
+
+use std::collections::{HashSet, VecDeque};
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+pub fn day_16_count_energized_tiles(input: &str) -> usize {
+    let layout = parse_2d_matrix(input);
+
+    let n = layout.len();
+    let m = layout[0].len();
+
+    print_2d_matrix(&layout);
+
+    let mut energized_squares: HashSet<(usize, usize)> = HashSet::new();
+    let mut explored_states: HashSet<((usize, usize), Direction)> = HashSet::new();
+    let mut next = VecDeque::from([((0, 0), Direction::Right)]);
+
+    loop {
+        if let Some(((i, j), direction)) = next.pop_front() {
+            explored_states.insert(((i, j), direction));
+            energized_squares.insert((i, j));
+
+            for next_state in match (layout[i][j], direction) {
+                ('.', Direction::Up) => vec![(move_2d((i, j), (-1, 0), (n, m)), Direction::Up)],
+                ('.', Direction::Down) => vec![(move_2d((i, j), (1, 0), (n, m)), Direction::Down)],
+                ('.', Direction::Left) => vec![(move_2d((i, j), (0, -1), (n, m)), Direction::Left)],
+                ('.', Direction::Right) => {
+                    vec![(move_2d((i, j), (0, 1), (n, m)), Direction::Right)]
+                }
+                ('/', Direction::Up) => vec![(move_2d((i, j), (0, 1), (n, m)), Direction::Right)],
+                ('/', Direction::Down) => vec![(move_2d((i, j), (0, -1), (n, m)), Direction::Left)],
+                ('/', Direction::Left) => vec![(move_2d((i, j), (1, 0), (n, m)), Direction::Down)],
+                ('/', Direction::Right) => vec![(move_2d((i, j), (-1, 0), (n, m)), Direction::Up)],
+                ('\\', Direction::Up) => vec![(move_2d((i, j), (0, -1), (n, m)), Direction::Left)],
+                ('\\', Direction::Down) => {
+                    vec![(move_2d((i, j), (0, 1), (n, m)), Direction::Right)]
+                }
+                ('\\', Direction::Left) => vec![(move_2d((i, j), (-1, 0), (n, m)), Direction::Up)],
+                ('\\', Direction::Right) => {
+                    vec![(move_2d((i, j), (1, 0), (n, m)), Direction::Down)]
+                }
+                ('|', Direction::Up) => vec![(move_2d((i, j), (-1, 0), (n, m)), Direction::Up)],
+                ('|', Direction::Down) => vec![(move_2d((i, j), (1, 0), (n, m)), Direction::Down)],
+                ('|', Direction::Left) => vec![
+                    (move_2d((i, j), (-1, 0), (n, m)), Direction::Up),
+                    (move_2d((i, j), (1, 0), (n, m)), Direction::Down),
+                ],
+                ('|', Direction::Right) => vec![
+                    (move_2d((i, j), (-1, 0), (n, m)), Direction::Up),
+                    (move_2d((i, j), (1, 0), (n, m)), Direction::Down),
+                ],
+                ('-', Direction::Up) => vec![
+                    (move_2d((i, j), (0, -1), (n, m)), Direction::Left),
+                    (move_2d((i, j), (0, 1), (n, m)), Direction::Right),
+                ],
+                ('-', Direction::Down) => vec![
+                    (move_2d((i, j), (0, -1), (n, m)), Direction::Left),
+                    (move_2d((i, j), (0, 1), (n, m)), Direction::Right),
+                ],
+                ('-', Direction::Left) => vec![(move_2d((i, j), (0, -1), (n, m)), Direction::Left)],
+                ('-', Direction::Right) => {
+                    vec![(move_2d((i, j), (0, 1), (n, m)), Direction::Right)]
+                }
+                (_, _) => unreachable!(),
+            }
+            .iter()
+            {
+                match next_state {
+                    (Ok((i, j)), direction) => {
+                        if !explored_states.contains(&((*i, *j), *direction)) {
+                            next.push_back(((*i, *j), *direction));
+                        }
+                    }
+                    (Err(_), _) => continue,
+                }
+            }
+        } else {
+            break;
+        }
+    }
+
+    energized_squares.len()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_16_count_energized_tiles() {
+        assert_eq!(
+            46,
+            day_16_count_energized_tiles(
+                r#".|...\....
+    |.-.\.....
+    .....|-...
+    ........|.
+    ..........
+    .........\
+    ..../.\\..
+    .-.-/..|..
+    .|....-|.\
+    ..//.|...."#
+            )
+        );
+    }
+}
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,14 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Move2DError {}
+
+impl fmt::Display for Move2DError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Moving out of matrix bounds!")
+    }
+}
+
 #[allow(dead_code)]
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
@@ -16,4 +27,30 @@ pub fn print_2d_matrix(matrix: &Vec<Vec<char>>) {
         println!("{}", row.iter().collect::<String>());
     }
     println!();
+}
+
+pub fn parse_2d_matrix(s: &str) -> Vec<Vec<char>> {
+    let mut ret = Vec::new();
+
+    for line in s.split("\n").map(|line| line.trim()) {
+        let mut row = Vec::new();
+        for c in line.chars() {
+            row.push(c);
+        }
+        ret.push(row);
+    }
+    ret
+}
+
+pub fn move_2d(
+    start: (usize, usize),
+    delta: (isize, isize),
+    size: (usize, usize),
+) -> Result<(usize, usize), Move2DError> {
+    let ((i, j), (dx, dy), (n, m)) = (start, delta, size);
+    let (new_x, new_y) = ((i as isize + dx), (j as isize + dy));
+    if new_x < 0 || new_x as usize >= n || new_y < 0 || new_y as usize >= m {
+        return Err(Move2DError {});
+    }
+    return Ok((new_x as usize, new_y as usize));
 }
